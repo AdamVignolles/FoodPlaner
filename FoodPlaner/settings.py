cc8200155a628099dbb83f90f5caffdf06eaaa3e
AdamVignolles <adam.vignolles@gmail.com>
2023-02-21 21:26:58 +0100

    Empty django app

--- FoodPlaner/settings.py
+++ FoodPlaner/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import django_heroku
+django_heroku.settings(locals())
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
