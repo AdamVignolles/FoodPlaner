49401a42c6469982b4be203ec27adb8c0e874dbe
AdamVignolles <adam.vignolles@gmail.com>
2023-02-22 15:58:23 +0100

    update

--- FoodPlaner/settings.py
+++ FoodPlaner/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-#import django_heroku
-#django_heroku.settings(locals())
 
 from pathlib import Path
 
