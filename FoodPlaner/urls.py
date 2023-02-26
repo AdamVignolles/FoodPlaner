be5e27acca9f1c3b48fccc05c6f4e540b78f661f
AdamVignolles <adam.vignolles@gmail.com>
2023-02-26 01:51:33 +0100

    update page acceuil

--- FoodPlaner/urls.py
+++ FoodPlaner/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import acceuil
+from .views import acceuil, loggin
 
 urlpatterns = [
     path('', acceuil, name="Acceuil"),
+    path('loggin/', loggin, name="Loggin"),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- FoodPlaner/views.py
+++ FoodPlaner/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import redirect, render
 
 def acceuil(request):
     return render(request, "FoodPlaner/Acceuil/index.html")
+
+def loggin(request):
+    return render(request, "FoodPlaner/Loggin/index.html")
