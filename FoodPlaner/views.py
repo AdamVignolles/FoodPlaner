f3b408fa2407dd9fd07820cf68220f2c417198e7
AdamVignolles <adam.vignolles@gmail.com>
2023-04-24 16:04:54 +0200

    add gestion planning by list

--- FoodPlaner/views.py
+++ FoodPlaner/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import redirect, render
 from django.conf import settings
 import json
 import os
-import urllib
-import requests
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
